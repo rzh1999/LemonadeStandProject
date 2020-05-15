5c772c48d2b7416399e9e86b3ff156d8a0516522
Bill <william.hine2019@gmail.com>
2020-05-15 11:24:15 -0400

    added human and computer class, created DisplayRules() and SetGameMode()

--- LemonadeStand/Game.cs
+++ LemonadeStand/Game.cs
@@ -8,5 +8,42 @@ namespace LemonadeStand
 {
     class Game
     {
+
+        public int GameMode;
+        public void DisplayRules()
+        {
+            Console.WriteLine($"Welcome to the lemonade stand game!");
+            Console.WriteLine($"In this game we will see how good of a lemonade stand tycoon you are!");
+            Console.WriteLine($"To play choose who will be playing, purchase goods, sell your lemonade its that simple.");
+            Console.WriteLine($"You may even want to create your own recipie and see what happens!!!");
+            Console.WriteLine();
+            
+        }
+
+        public void SetGameMode()
+        {
+            Console.WriteLine($"Enter Game mode:");
+            Console.WriteLine($"1. Single Player");
+            Console.WriteLine($"2. Multi-Player");
+            Console.WriteLine($"3. Player vs Computer");
+            string userInput = Console.ReadLine();
+
+            bool result;
+            int choice;
+            result = int.TryParse(userInput, out choice);
+            while (choice < 1 || choice > 3)
+            {
+                Console.WriteLine($"Please enter a valid choice");
+                userInput = Console.ReadLine();
+                result = int.TryParse(userInput, out choice);
+            }
+            GameMode = choice;
+        }
+
+        public void RunGame()
+        {
+            DisplayRules();
+            SetGameMode();
+        }
     }
 }
--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -10,6 +10,22 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
+            //Display Welcome and Rules
+            //Determine Game mode, Single player, MultiPlayer, or Human VS Robot
+            //Set players up names, default info etc
+            //Prompt user to select number of days to play 7, 14
+            //Begin to set up game
+            //Set and display intitial values to user 
+            //Prompt if player wants to create a recipie, if not display and use a default
+            //Display Shopping menu
+            //Prompt user to buy goods from shopping menu
+            //Display weaather and financial info available funds, cost of lemoneade per cup and list of starting ingredients
+            //Play game this is where the program takes over and decides how many cups were sold
+            // Create customers with % to buy and max value he is willing to pay
+            //End of day: display profit or loss for that day and profit or loss for cumaltive days
+            Game game = new Game();
+            game.RunGame();
+            Console.ReadLine();
         }
     }
 }
